ad9fa098ee8a7b02050ab944f99326a7f2152d4b
praja09 <dipaprajadpp@gmail.com>
2025-10-15 08:23:30 +0700

    Langkah 5: Buat Variabel text dan parameter di constructor

--- lib/red_text_widget.dart
+++ lib/red_text_widget.dart
@@ -1,8 +1,9 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
-class RedTextWidget extends StatelessWidget {
-  const RedTextWidget({Key? key}) : super(key: key);
-
+class RedTextWidget extends StatelessWidget 
+{final String text;
+const RedTextWidget({Key? key, required this.text}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return AutoSizeText(
